2e42cf0950c09362af37efb577bfe31c2291fad2
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2024-11-29 00:29:40 -0600

    Initial commit of Node.js server

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('ourServer.html', function(error,data){
+     if(error){
+        res.writeHead(404);
+        res.write("Error: file not found")
+     }else{
+        res.write(data);
+     
+     }
+        res.end();
+    });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+
+
+});
